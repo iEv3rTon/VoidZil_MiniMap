4aa136f3583fb05d0cbcafe2fb18008be58700f6
Ev3rtonAllakbar <46271347+Ev3rtonAllakbar@users.noreply.github.com>
2019-06-01 00:09:13 -0300

    Update and rename VoidZilMinimap-Master/minimap.user.js to  VoidZilMinimap-Master/minimap.user.js

--- VoidZilMinimap-Master/minimap.user.js
+++ VoidZilMinimap-Master/minimap.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @name         yOriowm Minimap
+// @name         VoidZil Minimap
 // @namespace    http://tampermonkey.net/
 // @version      2.0
 // @description  VoidZil_MiniMap
 // @author       yOriowm#1046
 // @match        https://pixelzone.io/*
 // @match        http://pixelzone.io/*
-// @homepage     https://github.com/yOriowm/ODNMinimap/
-// @updateURL    https://raw.githubusercontent.com/yOriowm/OrionMinimap/master/minimap.user.js
-// @downloadURL  https://raw.githubusercontent.com/yOriowm/OrionMinimap/master/minimap.user.js
+// @homepage     https://github.com/Ev3rtonAllakbar/VoidZil_MiniMap/
+// @updateURL    https://raw.githubusercontent.com/Ev3rtonAllakbar/VoidZil_MiniMap/master/VoidZilMinimap-Master/minimap.user.js
+// @downloadURL  https://raw.githubusercontent.com/Ev3rtonAllakbar/VoidZil_MiniMap/master/VoidZilMinimap-Master/minimap.user.js
 // @grant        none
 // ==/UserScript==
 
@@ -18,7 +18,7 @@ Number.prototype.between = function(a, b) {
   return this > min && this < max;
 };
 var range = 25;
-window.baseTepmlateUrl = 'https://raw.githubusercontent.com/yOriowm/OrionMinimap/master/';
+window.baseTepmlateUrl = 'https://raw.githubusercontent.com/Ev3rtonAllakbar/VoidZil_MiniMap/master/';
 
 window.addEventListener('load', function () {
     //Regular Expression to get coordinates out of URL
