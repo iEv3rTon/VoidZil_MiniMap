977d012fc7969e161dec66cd6602db8497c5604e
Ev3rtonAllakbar <46271347+Ev3rtonAllakbar@users.noreply.github.com>
2019-12-16 22:31:50 -0300

    Update minimap.user.js

--- minimap.user.js
+++ minimap.user.js
@@ -11,7 +11,7 @@
 // @grant        none
 // ==/UserScript==
 
-var baseTemplateUrl = 'https://raw.githubusercontent.com/Ev3rTonAllakbar/VoidZil_MiniMap/master/images/';
+var baseTemplateUrl = 'https://raw.githubusercontent.com/Ev3rTonAllakbar/VoidZil_MiniMap/master/';
 var vers = "VoiZil Minimap";
 var range = 6; //margin for showing the map window
 
